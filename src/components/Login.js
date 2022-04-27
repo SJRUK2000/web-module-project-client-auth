aa2207e1f8409bdd0257d37bde0e494bee0fd622
Sean-Ubele <seanjohnreedubeleknuckles@gmail.com>
2022-04-27 12:47:17 -0400

    bug: api not authing

--- src/components/Login.js
+++ src/components/Login.js
@@ -21,7 +21,7 @@ const {push} = useHistory();
         e.preventDefault();
         axios.post('http://localhost:9000/api/login', cred)
         .then(resp => {
-            localStorage.setItem("token", resp.payload);
+            localStorage.setItem("token", resp.data.payload);
             push('/friends')
         })
         .catch(err => {
