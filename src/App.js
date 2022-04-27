ccaac1be8b81f2dde08358960e6f9404aafdd3f3
Sean-Ubele <seanjohnreedubeleknuckles@gmail.com>
2022-04-27 12:16:29 -0400

    WORKING Links

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
+
 import React from 'react';
+
 import './App.css';
-import { BrowserRouter as Router, Route, Redirect} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Redirect, Link} from 'react-router-dom';
 import Login from './components/Login';
 import FriendsList from './components/FriendsList';
 import AddFriend from './components/AddFriend';
@@ -10,6 +12,14 @@ function App() {
   return (
     <Router>
       <div className="App">
+        <header>
+          <h2>Friends Database</h2>
+          <Link className='link' to='login'>Login</Link>
+          <Link className='link' to='friends'>Friends List</Link>
+          <Link className='link' to='friends'>Add Friends</Link>
+          <Link className='link' to='friends'>Logout</Link>
+
+        </header>
         <Route exact path={'/'}>
           <Login/>
         </Route>
